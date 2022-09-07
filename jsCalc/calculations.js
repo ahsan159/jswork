ff91728f442316dd56951b2897c21ff67af753d9
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2022-09-07 09:20:14 +0500

    got conversions working

--- jsCalc/calculations.js
+++ jsCalc/calculations.js
@@ -5,18 +5,112 @@ voltBtn.addEventListener("click", convertVolts);
 
 
 function convertVolts() {
-    let selectVal1 = document.getElementById("conversionVolt1")
-    let selectVal2 = document.getElementById("conversionVolt2")
-    let c1 = selectVal1.options[selectVal1.selectedIndex].text;
-    let c2 = selectVal2.options[selectVal2.selectedIndex].text;
-    console.log(c1);
-    console.log(c2);
-    if (val1.value == "" && val2.value == "") {
+        if (val1.value == "" && val2.value == "") {
         alert("You cannot leave not inputs blank");
         return;
     }
     else if (val1.value == "") {
         val1.value = val2.value;
+        let selectVal1 = document.getElementById("conversionVolt1")
+        let selectVal2 = document.getElementById("conversionVolt2")
+        let c2 = selectVal1.options[selectVal1.selectedIndex].text;
+        let c1 = selectVal2.options[selectVal2.selectedIndex].text;
+        console.log(c1);
+        console.log(c2);
+        let cT = document.getElementById("conversionText");
+        let conversionFactor = 1;
+        if (c1 == "micro-Volt(uV)") {
+            if (c2 == "micro-Volt(uV)") {
+                conversionFactor = 1;
+            }
+            else if (c2 == "milli-Volt(mV)") {
+                conversionFactor = 0.001;
+            }
+            else if (c2 == "Volt(V)") {
+                conversionFactor = 0.000001;
+            }
+            else if (c2 == "kilo-Volt(kV)") {
+                conversionFactor = 1E-9;
+            }
+            else if (c2 == "mega-Volt(MV)") {
+                conversionFactor = 1E-12;
+            }
+        }
+        else if (c1 == "milli-Volt(mV)") {
+            if (c2 == "micro-Volt(uV)") {
+                conversionFactor = 1000;
+            }
+            else if (c2 == "milli-Volt(mV)") {
+                conversionFactor = 1;
+            }
+            else if (c2 == "Volt(V)") {
+                conversionFactor = 0.001;
+            }
+            else if (c2 == "kilo-Volt(kV)") {
+                conversionFactor = 1E-6;
+            }
+            else if (c2 == "mega-Volt(MV)") {
+                conversionFactor = 1E-9;
+            }
+
+        }
+        else if (c1 == "Volt(V)") {
+            if (c2 == "micro-Volt(uV)") {
+                conversionFactor = 1000000;
+            }
+            else if (c2 == "milli-Volt(mV)") {
+                conversionFactor = 1000;
+            }
+            else if (c2 == "Volt(V)") {
+                conversionFactor = 1;
+            }
+            else if (c2 == "kilo-Volt(kV)") {
+                conversionFactor = 0.001;
+            }
+            else if (c2 == "mega-Volt(MV)") {
+                conversionFactor = 0.000001;
+            }
+
+        }
+        else if (c1 == "kilo-Volt(kV)") {
+            if (c2 == "micro-Volt(uV)") {
+                conversionFactor = 1000000000;
+            }
+            else if (c2 == "milli-Volt(mV)") {
+                conversionFactor = 1000000;
+            }
+            else if (c2 == "Volt(V)") {
+                conversionFactor = 1000;
+            }
+            else if (c2 == "kilo-Volt(kV)") {
+                conversionFactor = 1;
+            }
+            else if (c2 == "mega-Volt(MV)") {
+                conversionFactor = 1E-3;
+            }
+
+        }
+        else if (c1 == "mega-Volt(MV)") {
+            if (c2 == "micro-Volt(uV)") {
+                conversionFactor = 1E12;
+            }
+            else if (c2 == "milli-Volt(mV)") {
+                conversionFactor = 1E9;
+            }
+            else if (c2 == "Volt(V)") {
+                conversionFactor = 1E6;
+            }
+            else if (c2 == "kilo-Volt(kV)") {
+                conversionFactor = 1E3;
+            }
+            else if (c2 == "mega-Volt(MV)") {
+                conversionFactor = 1;
+            }
+        }
+        cT.textContent = String(conversionFactor);
+        val1.value = val2.value * conversionFactor;
+    }
+    else {        
         let selectVal1 = document.getElementById("conversionVolt1")
         let selectVal2 = document.getElementById("conversionVolt2")
         let c1 = selectVal1.options[selectVal1.selectedIndex].text;
@@ -113,10 +207,8 @@ function convertVolts() {
                 conversionFactor = 1;
             }
         }
-        cT.value = String(conversionFactor);
-        val1.value = val2.value * conversionFactor;
-    }
-    else {
-        val2.value = val1.value;
+        cT.textContent = conversionFactor.toString();
+        val2.value = val1.value * conversionFactor;
+        
     }
 }
