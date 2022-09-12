6d8fa5b97aae3a583a14c2774e63ec7b01558bf8
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2022-09-12 12:57:59 +0500

    first api interface

--- weatherApp/weatherapi.js
+++ weatherApp/weatherapi.js
@@ -1,8 +1,10 @@
 let btn = document.getElementById("submitt");
+let cityh1 = document.getElementById("cityh1");
+let temperatureh1 = document.getElementById("temph1");
 // btn.addEventListener("click", callurl);
 
 
-let url = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=New%20York";
+let url = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=Lahore";
 
 let m;
 async function funcRequest(url) {
@@ -14,6 +16,14 @@ async function funcRequest(url) {
             // Here we can use the response Data
             console.log(data);
             m = data;
+            try {
+            cityh1.textContent = m.location.name;
+            temperatureh1.textContent = m.current.temperature;
+            }
+            catch (e) {
+                console.log(m.error.info);
+            }
+
         })
         .catch(function (error) {
             console.log(error);
