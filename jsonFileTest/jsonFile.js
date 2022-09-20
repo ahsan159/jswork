5d06d609027084c39d782c29afd50a7a50017d80
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2022-09-20 16:41:43 +0500

    added capitals instead of all city list

--- jsonFileTest/jsonFile.js
+++ jsonFileTest/jsonFile.js
@@ -1,4 +1,17 @@
 let dataSaved = null;
-fetch('./cities.json')
+let cityDropDown = document.getElementById("citiesAll")
+fetch('./capitals.json')
     .then((response) => response.json())
-    .then((jsonData) => (dataSaved = jsonData));
+    .then((jsonData) => {
+        dataSaved = jsonData;
+        let dStr = null;
+        for (let i = 0; i < dataSaved.length; i++) {
+            let v = i.toString();
+            let c = dataSaved[i].name;
+            dStr = dStr + `
+            <option value="${v}">${dataSaved[i].city}</option>
+            `;
+            // console.log(dataSaved[i].name);
+        }
+        cityDropDown.innerHTML = dStr;
+    });
