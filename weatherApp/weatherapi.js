68593a65f4054724ead40b06a5cb5aad9f33222d
masaddique <eemasaddique@gmail.com>
2022-09-20 22:53:35 +0500

    changed weather app

--- weatherApp/weatherapi.js
+++ weatherApp/weatherapi.js
@@ -1,3 +1,6 @@
+let searchBtn = document.getElementById("submitSearch");
+let searchCity = document.getElementById("citySearch");
+searchBtn.addEventListener("click", shiftCities);
 let btn = document.getElementById("submitt");
 let cityh1 = document.getElementById("cityh1");
 let temperatureh1 = document.getElementById("temph1");
@@ -10,24 +13,48 @@ let windSpeed = document.getElementById("windh1");
 let windDirection = document.getElementById("direh1");
 weatherImage.src = "./sunny.jpg";
 
+let cityf1 = document.getElementById("cityf1")
 let imagf1 = document.getElementById("imagf1");
 let tempf1 = document.getElementById("tempf1");
 let humdf1 = document.getElementById("humdf1");
 let precf1 = document.getElementById("precf1");
 
+let cityf2 = document.getElementById("cityf2")
 let imagf2 = document.getElementById("imagf2");
 let tempf2 = document.getElementById("tempf2");
 let humdf2 = document.getElementById("humdf2");
 let precf2 = document.getElementById("precf2");
 
+let cityf3 = document.getElementById("cityf3")
 let imagf3 = document.getElementById("imagf3");
 let tempf3 = document.getElementById("tempf3");
 let humdf3 = document.getElementById("humdf3");
 let precf3 = document.getElementById("precf3");
 // btn.addEventListener("click", callurl);
 
+let city = "Lahore";
+let city1 = "Karachi";
+let city2 = "Islamabad";
+let city3 = "London";
 
-let url = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=Lahore";
+let url;
+let url1;
+let url2;
+let url3;
+
+url = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city;
+url1 = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city1;
+url2 = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city2;
+url3 = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city3;
+
+let resp = funcRequest(url);
+let resp1 = getOtherCities(url1, tempf1, humdf1, precf1, imagf1);
+let resp2 = getOtherCities(url2, tempf2, humdf2, precf2, imagf2);
+let resp3 = getOtherCities(url3, tempf3, humdf3, precf3, imagf3);
+
+cityf1.textContent = city1;
+cityf2.textContent = city2;
+cityf3.textContent = city3;
 
 let m;
 async function funcRequest(url) {
@@ -61,4 +88,42 @@ async function funcRequest(url) {
         });
 }
 
-let resp = funcRequest(url);
+async function getOtherCities(url, temp, hum, perc, imag) {
+    await fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            temp.textContent = data.current.temperature + "°C";
+            hum.textContent = data.current.humidity + "%";
+            perc.textContent = data.current.precip + "%";
+            imag.src = "./smoke.jpg";
+        });
+}
+
+function shiftCities() {
+    // console.log("here");
+    city3 = city2;
+    city2 = city1;
+    city1 = city;
+    city = searchCity.value;
+
+    // console.log(city);
+
+    url = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city;
+    url1 = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city1;
+    url2 = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city2;
+    url3 = "http://api.weatherstack.com/current?access_key=4a89e072d9e3836e7989f2b15332e7c5&query=" + city3;
+
+    resp = funcRequest(url);
+    resp1 = getOtherCities(url1, tempf1, humdf1, precf1, imagf1);
+    resp2 = getOtherCities(url2, tempf2, humdf2, precf2, imagf2);
+    resp3 = getOtherCities(url3, tempf3, humdf3, precf3, imagf3);
+
+    cityh1.textContent = city;
+    cityf1.textContent = city1;
+    cityf2.textContent = city2;
+    cityf3.textContent = city3;
+
+    return false;
+}
