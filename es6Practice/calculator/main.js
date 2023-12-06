5b4ea056b9d665d71381ed68f02ef445ee3cb65c
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-06 14:57:26 +0500

    working calculator

--- es6Practice/calculator/main.js
+++ es6Practice/calculator/main.js
@@ -1,14 +1,46 @@
 var expr = "";
 
+var num1 = "";
+var num2 = "";
+var oper = "";
+var ans = "";
+
+function expressionOper(bIn)
+{
+    console.log(bIn);
+    if (num1=="")
+    {
+        num1 = expr;
+        oper = bIn;
+        expr = "";
+    }
+    else if (num1!="")
+    {
+        num2 = expr;
+        expr = num1+oper+num2;        
+        console.log(expr);
+        document.getElementById("disp").innerText = eval(expr);
+        num1 = eval(expr);
+        oper = bIn;
+        expr = "";
+    }
+}
+
 function expression(bIn) {
     expr = expr + bIn;
     console.log(expr);
 }
 
-function evaluateExp() {
-    if (expr != "") {
+function evaluateExp() {    
+    console.log(expr);
+    if (num1!="") {
+        expr = num1 + oper + expr;
         console.log(expr);
         document.getElementById("disp").innerText = eval(expr);
-        expr = "";
+        ans = eval(expr);
+        expr = ans;
+        num1 = "";
+        num2 = "";
+        oper = "";
     }
 }
