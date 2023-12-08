dab9bac39eea3ea7c892bcee1e762d70e2584e11
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-08 16:11:41 +0500

    date data inserted

--- es6Practice/calender/main.js
+++ es6Practice/calender/main.js
@@ -1,5 +1,6 @@
 var d = new Date();
 var monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+var dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
 var dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 window.onload = ()=>
 {
@@ -31,4 +32,30 @@ function updateCalender(cmd)
         document.getElementById("monthText").innerText = monthNames[d.getMonth()];                
         document.getElementById("yearText").innerText = d.getFullYear();  
     }
+    var tableID = document.getElementById("ctable");
+    let td = " ";
+    let tr = `            <tr>
+                            <th style="color:red">S</th>
+                            <th>M</th>
+                            <th>T</th>
+                            <th>W</th>
+                            <th>T</th>
+                            <th>F</th>
+                            <th>S</th>
+                        </tr>`;
+    for(let i = 0; i < dayCount[d.getMonth()];i++)
+    {        
+        if (i%7==0)
+        {
+            td=td+"<tr>";
+        }
+        td = td + "<td onclick=\"console.log('me')\">" + (i+1) + "</td>";
+        if (i%7==6)
+        {
+            td=td + "</tr>";
+        }
+        // console.log(i);
+    }
+    // console.log(td);
+    tableID.innerHTML = tr + td;
 }
