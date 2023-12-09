bcf5846cce7e0cc287a16e0c3d4a347a77e35241
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-09 14:01:43 +0500

    added todo list table using grid

--- es6Practice/calender/main.js
+++ es6Practice/calender/main.js
@@ -6,6 +6,7 @@ window.onload = () => {
     document.getElementById("monthText").innerText = monthNames[d.getMonth()];
     document.getElementById("yearText").innerText = d.getFullYear();
     updateCalender('print');
+    document.getElementById("todayis").innerText = d.toLocaleDateString();
 }
 
 function printCalender() {
@@ -13,19 +14,19 @@ function printCalender() {
     let td = " ";
     // add the header
     let tr = `            <tr>
-                            <th style="color:red">S</th>
-                            <th>M</th>
-                            <th>T</th>
-                            <th>W</th>
-                            <th>T</th>
-                            <th>F</th>
-                            <th>S</th>
+                            <th style="color:red" class=\"weekHeader\">S</th>
+                            <th class=\"weekHeader\">M</th>
+                            <th class=\"weekHeader\">T</th>
+                            <th class=\"weekHeader\">W</th>
+                            <th class=\"weekHeader\">T</th>
+                            <th class=\"weekHeader\">F</th>
+                            <th class=\"weekHeader\">S</th>
                         </tr>`;
 
     // save today 
     let dToday = new Date();
     dToday = dToday.toLocaleDateString();
-    console.log(dToday);
+    // console.log(dToday);
     // current print month 
     let cMonth = d.getMonth();
     // find the dayCount in month and correct for leap year                        
@@ -49,6 +50,7 @@ function printCalender() {
     // for(let i = 0,j=0,k=0; i < dDayCount;j++)
     for (let dn = dfirstDay, k = 1, j = 0; dn <= dlastDay; dn.setDate(dn.getDate() + 1), j++) {
         // start the new row with new week
+        let id = dn.toLocaleDateString();
         if (j % 7 == 0) {
             td = td + "<tr>";
         }
@@ -56,17 +58,17 @@ function printCalender() {
         if (dn.toLocaleDateString()==dToday)
         {
             // today display with different class
-            td = td + "<td class=\"today\" onclick=\"console.log('me')\">" + dn.getDate() + "</td>";
+            td = td + `<td class="today" id="${id}" onclick="addTaskFunction(event)">  ${dn.getDate()}  </td>`;
         }
         else if (dn.getMonth() == cMonth) 
         {
             // current month
-            td = td + "<td class=\"currentMonthText\" onclick=\"console.log('me')\">" + dn.getDate() + "</td>";
+            td = td + `<td class="currentMonthText" id="${id}" onclick="addTaskFunction(event)">  ${dn.getDate()}  </td>`;
         }         
         else 
         {
             // remaining month to cover weeks
-            td = td + "<td class=\"dateText\" onclick=\"console.log('me')\">" + dn.getDate() + "</td>";
+            td = td + `<td class="dateText" id="${id}" onclick="addTaskFunction(event)">  ${dn.getDate()}  </td>`;
         }
         // end the row with ending week
         if (j % 7 == 6) {
@@ -98,4 +100,31 @@ function updateCalender(cmd) {
         document.getElementById("yearText").innerText = d.getFullYear();
     }
     printCalender();
+}
+
+function addTaskFunction(e)
+{
+    // console.log("This is the function");
+    // console.log(arguments.callee);
+    // console.log(e.target.id);
+    let dr = document.createElement("div");
+    dr.setAttribute("class","row");
+
+    let ds = document.createElement("div");
+    ds.setAttribute("class","box tableentry");
+    ds.innerText=document.getElementById("todotable").childElementCount-2;
+    let dd = document.createElement("div");
+    dd.setAttribute("class","box tableentry");
+    dd.innerText=e.target.id;    
+    let dt = document.createElement("div");
+    dt.setAttribute("class","box tableentry");
+    dt.innerText = "this is the task text";
+
+    dr.appendChild(ds);
+    dr.appendChild(dd);
+    dr.appendChild(dt);
+
+    document.getElementById("todotable").appendChild(dr);
+    // console.log(document.getElementById("todotable").innerHTML);
+
 }
