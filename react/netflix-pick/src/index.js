a746dd5e8de603d14c7d6c2e19e67fa7146edb55
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-28 15:00:10 +0500

    Simple website using react

---
+++ react/netflix-pick/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <>
+    <h1>My Top 5 Netflix Pick</h1>
+    <p> List of 5 Best Series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra Curricular</li>
+      <li>My Holo Love</li>
+      <li>My First-2 Love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </>,document.getElementById('root')
+)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
