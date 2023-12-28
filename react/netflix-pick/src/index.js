cb9930b5bd6e45564ffa2c65dfc9b169c452a555
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-28 15:19:54 +0500

    use of tempelate literal

--- react/netflix-pick/src/index.js
+++ react/netflix-pick/src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom'
 
 // uisng JSX (Javascript Expressions for list)
 
+const fname = 'Ahsan';
+const lname = 'Saddique';
+
 const nameArray = [
   'Dark',
   'Extra Curricular',
   'My Holo Love',
   'My First-2 Love',
   'Mr Robot'
-]
+];
 
 ReactDOM.render(
   <>
     <h1>My Top 5 Netflix Pick</h1>
-    <p> List of 5 Best Series</p>
+    {/* below line is using tempelate literal   */}
+    <p>{`I am ${fname} ${lname} and this is my List of 5 Best Series`}</p>
     <ol>
       <li>{nameArray[0]}</li>
       <li>{nameArray[1]}</li>
@@ -23,7 +27,7 @@ ReactDOM.render(
       <li>{nameArray[4]}</li>
     </ol>
   </>,document.getElementById('root')
-)
+);
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
