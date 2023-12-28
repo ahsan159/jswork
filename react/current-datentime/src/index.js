2c8b94871897e6b1c666726f9fdd86de020f8e29
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-28 15:56:37 +0500

    added css styles to app

--- react/current-datentime/src/index.js
+++ react/current-datentime/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const t = new Date();
-const name = 'Ahsan';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const imgLink1 = 'https://picsum.photos/400/400';
-const imgLink2 = 'https://picsum.photos/300/400';
-const imgLink3 = 'https://picsum.photos/200/400';
+const name = "Ahsan";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const imgLink1 = "https://picsum.photos/400/400";
+const imgLink2 = "https://picsum.photos/300/400";
+const imgLink3 = "https://picsum.photos/200/400";
 // root.render(
 //   <React.StrictMode>
 //     <App />
@@ -18,22 +18,20 @@ const imgLink3 = 'https://picsum.photos/200/400';
 
 root.render(
   <>
-    <h1>Today</h1>
-    <p>My name is {name}</p>
-    <p>{`Current Time is ${t.toLocaleTimeString()}`}</p>
-    <p>{`Current Date is ${t.toLocaleDateString()}`}</p>
-    <img src={imgLink1}
-     alt='RandomImages'></img>
-     <img src={imgLink2}
-     alt='RandomImages'></img>
-     <img src={imgLink3}
-     alt='RandomImages'></img>     
-     <a href='http://www.google.com/'>
-      {/* <img src={imgLink1}></img> */}
-      This will direct you to another link
-     </a>
+    <h1 className="heading">Today</h1>
+    {/* <p>My name is {name}</p> */}
+    <div className="datentime">
+      <p>{`Current Time is ${t.toLocaleTimeString()}`}</p>
+      <p>{`Current Date is ${t.toLocaleDateString()}`}</p>
+    </div>
+    <div className="images">
+    <img src={imgLink1} alt="RandomImages"></img>
+    <img src={imgLink2} alt="RandomImages"></img>
+    <img src={imgLink3} alt="RandomImages"></img>
+    </div>
+    <a href="http://www.google.com/">This will direct you to another link</a>
   </>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
