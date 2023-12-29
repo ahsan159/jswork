7129ccd5680aa99a8cf851573e7e3670fe1bbe1a
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-12-29 15:07:07 +0500

    components created

---
+++ react/components-generation/src/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Heading()
+{
+    return (
+        <>
+            <h1>Generaing Components</h1>
+            <p>I taught following courses last 2 years</p>
+        </>
+    );
+}
+
+export default Heading;
---
+++ react/components-generation/src/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <>
+      <ol>
+        <li>Mathematical Methods for Physics</li>
+        <li>Electromagnetic Theory</li>
+        <li>Modern Physics 1</li>
+      </ol>
+    </>
+  );
+}
+
+export default List;
