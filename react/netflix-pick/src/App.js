2ad09030a655778a16ad575a4025910ccd2c3b4e
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-01-02 09:37:19 +0500

    moved div to App.js as industry practice

--- react/netflix-pick/src/App.js
+++ react/netflix-pick/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import Card from './Card';
+import SData from './Data';
 import './App.css';
+// import logo from './logo.svg';
+
+let ncards = (val) => {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    ></Card>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+    <h1 className="heading_style">List of Top5 Netflix Series 2020</h1>
+    {/* using simple function   */}
+    {/* {SData.map(ncards)} */}
+    {/* annonymus fat arrow function */}
+    {/* a unique "key" is a must for every child react element */}
+    {SData.map((val) => {
+      return (
+        <Card
+          key={val.id}
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        ></Card>
+      );
+    })}
+    {/* <Card
+      imgsrc={SData[1].imgsrc}
+      title={SData[1].title}
+      sname={SData[1].sname}
+      link={SData[1].link}
+    ></Card>
+    <Card
+      imgsrc={SData[2].imgsrc}
+      title={SData[2].title}
+      sname={SData[2].sname}
+      link={SData[2].link}
+    ></Card>
+    <Card
+      imgsrc={SData[3].imgsrc}
+      title={SData[3].title}
+      sname={SData[3].sname}
+      link={SData[3].link}
+    ></Card>
+    <Card
+      imgsrc={SData[4].imgsrc}
+      title={SData[4].title}
+      sname={SData[4].sname}
+      link={SData[4].link}
+    ></Card> */}
+  </>
   );
 }
 
--- react/netflix-pick/src/index.js
+++ react/netflix-pick/src/index.js
@@ -3,66 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Card from "./Card";
-import SData from "./Data";
+import App from "./App";
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-let ncards = (val) => {
-  return (
-    <Card
-      imgsrc={val.imgsrc}
-      title={val.title}
-      sname={val.sname}
-      link={val.link}
-    ></Card>
-  );
-};
+
 
 root.render(
-  // below code will develop card from scratch without using bootstrap
-  <>
-    <h1 className="heading_style">List of Top5 Netflix Series 2020</h1>
-    {/* using simple function   */}
-    {/* {SData.map(ncards)} */}
-    {/* annonymus fat arrow function */}
-    {/* a unique "key" is a must for every child react element */}
-    {SData.map((val) => {
-      return (
-        <Card
-          key={val.id}
-          imgsrc={val.imgsrc}
-          title={val.title}
-          sname={val.sname}
-          link={val.link}
-        ></Card>
-      );
-    })}
-    {/* <Card
-      imgsrc={SData[1].imgsrc}
-      title={SData[1].title}
-      sname={SData[1].sname}
-      link={SData[1].link}
-    ></Card>
-    <Card
-      imgsrc={SData[2].imgsrc}
-      title={SData[2].title}
-      sname={SData[2].sname}
-      link={SData[2].link}
-    ></Card>
-    <Card
-      imgsrc={SData[3].imgsrc}
-      title={SData[3].title}
-      sname={SData[3].sname}
-      link={SData[3].link}
-    ></Card>
-    <Card
-      imgsrc={SData[4].imgsrc}
-      title={SData[4].title}
-      sname={SData[4].sname}
-      link={SData[4].link}
-    ></Card> */}
-  </>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
 );
 
 reportWebVitals();
