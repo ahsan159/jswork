ce43b7bb5a5c71efc8f71d572ec3ee2b22b2c67e
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-01-03 08:53:42 +0500

    updated slot machine app

--- react/netflix-pick/src/index.js
+++ react/netflix-pick/src/index.js
@@ -7,12 +7,10 @@ import App from "./App";
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
 root.render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
--- react/slot_machine/src/App.js
+++ react/slot_machine/src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDom from "react-dom/client";
 // import logo from './logo.svg';
 import "./App.css";
+import SData from "./SData";
+
+var r1 = Math.random() * (SData.length - 1);
+r1 = r1.toFixed(0);
+var r2 = Math.random() * (SData.length - 1);
+r2 = r2.toFixed(0);
+var r3 = Math.random() * (SData.length - 1);
+r3 = r3.toFixed(0);
 
 const SlotM = (props) => {
   let { x, y, z } = props;
-
+  // console.log(`${SData.length} ${r1} ${r2} ${r3}`);
   if (x === y && z === x) {
     return (
       <>
@@ -42,7 +50,7 @@ function App() {
         🎰{" "}
       </h1>
       <div className="slotBody">
-        <SlotM key={1} x={"😀"} y={"😀"} z={"😆"} />
+        <SlotM key={1} x={SData[r1]} y={SData[r2]} z={SData[r3]} />
         <SlotM key={2} x={"😀"} y={"😀"} z={"😀"} />
         <SlotM key={3} x={"😀"} y={"😀"} z={"🎅"} />
         <div className="bottomLine"></div>
