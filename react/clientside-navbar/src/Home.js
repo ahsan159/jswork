67538f1e05e9a7051d794ef700aea0e1cdef39b8
masaddique <eemasaddique@gmail.com>
2024-01-31 23:40:12 +0500

    updated style sheets

--- react/clientside-navbar/src/Home.js
+++ react/clientside-navbar/src/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Welcome to the home this is my home page from where ever you are you are gonna find this</p>
+    </>
+  );
 };
 
 export default Home;
