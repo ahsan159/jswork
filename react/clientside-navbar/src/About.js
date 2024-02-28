4e7b8bb38427bb4ebf3b0ff7694cb536c607b317
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-02-28 15:43:14 +0500

    updated about

--- react/clientside-navbar/src/About.js
+++ react/clientside-navbar/src/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About</h1>
+      <p>updating the about page.</p>
+      <p>
+         also want to tell your that we can not put the Navbar after the Routes because it will cause navigation bar placed after content.
+      </p>
+    </>
+  );
 };
 
 export default About;
