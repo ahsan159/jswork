13de4f2b4bd758f4175659d3fabccaa5f5e447ce
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-03-01 09:16:03 +0500

    Err404

--- react/clientside-navbar/src/NoPageFound.js
+++ react/clientside-navbar/src/NoPageFound.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./NoPageFound.css";
 
 const NoPageFound = () => {
   let loc = useLocation();
-  return (<h1> Page Not Found {loc.pathname} </h1>);
+  return (
+    <>
+      <div className="noPage">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <p>Cannot find resource "{loc.pathname}"</p>
+      </div>
+    </>
+  );
 };
 
 export default NoPageFound;
