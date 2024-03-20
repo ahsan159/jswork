581e589cff14d7eed64bc89a223851ad11823d7e
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-03-20 11:33:25 +0500

    using react-multistep

--- one-card-generator/src/Home.js
+++ one-card-generator/src/Home.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
+import MultiStep from 'react-multistep';
+import { multiStepStyles } from "./multistepStyles";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
+import Step3 from "./Step3";
 import "./Home.css";
 
 const Home = () => {
-  let [cStep, updateStep] = useState(1);
+  // let [cStep, updateStep] = useState(1);
   return (
     <>
       <div className="main">
-        <h1> This home Page</h1>
+      <MultiStep styles={multiStepStyles} showTitles={true}
+      prevButton={{title:'BackStep',style:{background:'red'}}}       
+      nextButton={{title:'NextStep',style:{background:'green'}}}
+      direction="row">      
+        <Step1 title='step1'></Step1>
+        <Step2 title='step2'></Step2>
+        <Step3 title='step3'></Step3>
+      </MultiStep>
+        {/* <h1> This home Page</h1>
         { (cStep==1) && <Step1></Step1> }
-        { (cStep==2) && <Step2></Step2> }
-        {/* <Step1></Step1> */}
+        { (cStep==2) && <Step2></Step2> }        
         <div className="buttonDiv">
           <button
             onClick={() => {
@@ -29,7 +39,7 @@ const Home = () => {
           >
             Next
           </button>
-        </div>
+        </div> */}
       </div>
     </>
   );
