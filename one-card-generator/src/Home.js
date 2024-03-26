618b93a08ce5422516131bc78c9cad75c32d1595
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-03-26 13:43:02 +0500

    added Step Processing for step1 and step2

--- one-card-generator/src/Home.js
+++ one-card-generator/src/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import MultiStep from "react-multistep";
-import { multiStepStyles } from "./multistepStyles";
+// import MultiStep from "react-multistep";
+// import { multiStepStyles } from "./multistepStyles";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
@@ -22,11 +22,12 @@ const stepperSize = {
   paddingTop: "15px",
 };
 
+
 const Home = () => {
   let [cStep, updateStep] = useState(1);
-  let [username, updateuserName] = useState("hello213");
-  let [password1, updatePassword1] = useState("hello1243");
-  let [password2, updatePassword2] = useState("");
+  let [username, updateuserName] = useState("");
+  let [password, updatePassword] = useState([]);
+
   let stepperSteps = ["username", "password", "step3", "review", "final"];
 
   return (
@@ -51,8 +52,20 @@ const Home = () => {
           })}
         </Stepper>
         {/* <h1> This home Page</h1> */}
-        {cStep === 1 && <Step1 key="a" username={username} updateFunction={updateuserName}></Step1>}
-        {cStep === 2 && <Step2 key="b"></Step2>}
+        {cStep === 1 && (
+          <Step1
+            key="a"
+            username={username}
+            updateFunction={updateuserName}
+          ></Step1>
+        )}
+        {cStep === 2 && (
+          <Step2
+            key="b"
+            password={password}
+            updateFunction={updatePassword}
+          ></Step2>
+        )}
         {cStep === 3 && <Step3 key="c"></Step3>}
         <div className="buttonDiv">
           <Button
@@ -72,15 +85,35 @@ const Home = () => {
             endIcon={<ArrowRightIcon />}
             onClick={() => {
               // alert(`This is Next  ${cStep}`);
-              if (cStep===1)
-              {
+              let valid = false;
+              // Step 1 Processing
+              if (cStep === 1) {
                 console.log(`current User name is ${username}`);
+                valid = true;
+                if (username.length === 0) {
+                  alert("Kindly Provide Valid Username");
+                  valid = false;
+                }
               }
-              updateStep(cStep + 1);
-              if (cStep > 5) {
-                updateStep(5);
+              // Step 2 Processing
+              if (cStep === 2) {
+                console.log(
+                  `current password is ${password[0]} and ${password[1]}`
+                );
+                if (password[0].length > 0 && password[0] === password[1]) {
+                  valid = true;
+                } else {
+                  alert("invalid password");
+                }
               }
-              console.log(cStep);
+              // Step 3 Processing
+              if (valid) {
+                updateStep(cStep + 1);
+                if (cStep > 5) {
+                  updateStep(5);
+                }
+                console.log(cStep);
+              }
             }}
           >
             Next
--- one-card-generator/src/Step2.js
+++ one-card-generator/src/Step2.js
@@ -1,14 +1,28 @@
 import React from "react";
-import "./Step.css"
+import "./Step.css";
 
-const Step2 = () => {
+const Step2 = (props) => {
   return (
     <>
       <div className="mainBox">
         <h2>Step2/3</h2>
         <h1>Enter your Password?</h1>
-        <input placeholder="Enter your password" type="Password"></input>
-        <input placeholder="Enter your password again" type="Password"></input>
+        <input
+          placeholder="Enter your password"
+          type="Password"
+          value={props.password[0]}
+          onChange={(event) => {
+            props.updateFunction([event.target.value, props.password[1]]);
+          }}
+        ></input>
+        <input
+          placeholder="Enter your password again"
+          type="Password"
+          value={props.password[1]}
+          onChange={(event) => {
+            props.updateFunction([props.password[0], event.target.value]);
+          }}
+        ></input>
       </div>
     </>
   );
