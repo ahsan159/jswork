5f78fbf7a7948a387dc0824656d819ba1c70d8a1
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-04-04 09:39:42 +0500

    added styling

--- react-weather-app/src/Weather.js
+++ react-weather-app/src/Weather.js
@@ -13,14 +13,18 @@ const Weather = () => {
           type="search"
           variant="outlined"
           placeholder="Search City"
-          sx={{marginTop:"30px"}}
+          sx={{marginTop:"30px",backgroundColor:"white"}}
           onChange={(event) => {
             setCity(event.target.value);
           }}
         ></TextField>
         <h1 className="cityName">{city}</h1>
-        <h2 className="temperatureCurrent">29.6♨️</h2>
-
+        <h2 className="temperatureCurrent">29.6︒C</h2>
+        <div className="temperatureMaxMin">
+          <h2>24.6︒C</h2>
+          <h2>|</h2>
+          <h2>33.1︒C</h2>
+        </div>
       </div>
     </>
   );
