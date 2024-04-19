c3e62e3125b5e4ef59a58dd5e28011bbb773e576
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-04-19 14:24:39 +0500

    testing axios post command

--- datatable/src/TableComponent.js
+++ datatable/src/TableComponent.js
@@ -30,12 +30,20 @@ const rows = [
 let TableComponent = () => {
   let [kingname, updatekingname] = useState([]);
   let [indexId, updateIndexId] = useState(0);
+  let [monarchName, updateMonarchName] = useState("");
+  let [reignStart, updateReginStart] = useState(0);
+  let [reignEnd, updateReginEnd] = useState(0);
+
+  const deleteLast = () => {
+    console.log("this will delete last data");
+  };
 
   const getrequest = () => {
     console.log(`requesting using axios at ${dayjs(Date.now())}`);
     axios
       .get(
-        `http://localhost:8000/temp?index=${indexId}&name=panadol&id=0c23544`
+        `http://localhost:8000/temp?index=${indexId}&start=${reignStart}`
+        `http://localhost:8000/temp?index=${indexId}&start=${reignStart}`
       )
       .then((res) => {
         // save and log the result
@@ -67,7 +75,37 @@ let TableComponent = () => {
           }}
           placeholder="Enter the index!!!"
         ></input>
-        <button onClick={getrequest}>Request</button>
+        <div className="buttonDiv">
+          <button onClick={getrequest}>Request</button>
+          <button onClick={deleteLast}>Delete Last</button>
+        </div>
+        <div className="addData">
+          <input
+          style={{'width':'90px'}}
+            type="text"
+            value={monarchName}
+            onChange={(evt) => {
+              updateMonarchName(evt.target.value);
+            }}
+            placeholder="Enter Monarch name"
+          ></input>
+          <input
+            type="number"
+            value={reignStart}
+            onChange={(evt) => {
+              updateReginStart(evt.target.value);
+            }}
+            placeholder="Enter start"
+          ></input>
+          <input
+            type="number"
+            value={reignEnd}
+            onChange={(evt) => {
+              updateReginEnd(evt.target.value);
+            }}
+            placeholder="Enter start"
+          ></input>
+        </div>
         {/* <h2>{kingname.name}</h2>
         <h2>{`${kingname.start} ~ ${kingname.end}`}</h2> */}
         <DataGridComponent key={0} datatable={kingname}></DataGridComponent>
