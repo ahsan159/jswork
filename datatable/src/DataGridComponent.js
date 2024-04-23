9192844f83aaaff75d4d7ba2f2f849abb58985c9
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-04-23 14:34:33 +0500

    implemented delete using axios

--- datatable/src/DataGridComponent.js
+++ datatable/src/DataGridComponent.js
@@ -1,42 +1,53 @@
 import { React, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, renderActionsCell } from "@mui/x-data-grid";
 import axios from "axios";
 import dayjs from "dayjs";
 import "./TableComponent.css";
+import { extractValidationProps } from "@mui/x-date-pickers/internals";
 // import Clock from "react-live-clock";
 
-const rows = [
-  { name: "Elizabeth", start: 1565, end: 1603, id: 0 },
-  { name: "James I", start: 1603, end: 1625, id: 1 },
-  { name: "Charles I", start: 1625, end: 1649, id: 2 },
-  {
-    name: "Cromwell",
-    start: 1649,
-    end: 1660,
-    commonwealth: true,
-    id: 3,
-  },
-  { name: "Charles II", start: 1660, end: 1685, id: 4 },
-  { name: "James II", start: 1685, end: 1689, id: 5 },
-  { name: "W&M", start: 1689, end: 1702, id: 6 },
-  { name: "Anne", start: 1702, end: 1714, id: 7 },
-  { name: "George I", start: 1714, end: 1727, id: 8 },
-  { name: "George II", start: 1727, end: 1760, id: 9 },
-  { name: "George III", start: 1760, end: 1820, id: 10 },
-  { name: "George IV", start: 1820, end: 1820, id: 11 },
-];
+const sendDelete = (params) => {
+  console.log(params);
+  axios
+    .delete("http://localhost:8000/api/medicines", {data:params})
+    .then((response) => {
+      console.log(response);
+      alert(response.data.message);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 const columns = [
   { field: "id", headerName: "ID" },
   { field: "name", headerName: "Name" },
   { field: "start", headerName: "Start" },
   { field: "end", headerName: "end" },
+  {
+    field: "delete",
+    headerName: "Delete",
+    Selection: false,
+    renderCell: (params) => {
+      return (
+        <button
+          className="tableButton"
+          onClick={() => {
+            alert("I am clicked to delete the selected row");
+            console.log(params.row);            
+            sendDelete(params.row);
+          }}
+        >
+          Delete
+        </button>
+      );
+    },
+  },
 ];
 
 const DataGridComponent = (props) => {
   return (
     <>
-      
       {/* <h1>{dataTable.length}</h1> */}
       <DataGrid
         columns={columns}
@@ -46,6 +57,7 @@ const DataGridComponent = (props) => {
         }}
         pageSizeOptions={[5, 10]}
         checkboxSelection={true}
+        disableRowSelectionOnClick={true}
       ></DataGrid>
     </>
   );
