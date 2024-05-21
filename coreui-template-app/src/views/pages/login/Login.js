d635893bd7740f46749d54e62a7c96abb428e43b
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-05-21 15:04:07 +0500

    successfully authenticated login

--- coreui-template-app/src/views/pages/login/Login.js
+++ coreui-template-app/src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate, useNavigation } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -15,8 +15,47 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import axios from 'axios'
 
 const Login = () => {
+  const [userDetails, setUserDetails] = useState({
+    email: '',
+    password: '',
+  })
+  const uNavigate = useNavigate()
+
+  const onUserDetailChange = (evt) => {
+    const cname = evt.target.name
+    const cvalue = evt.target.value
+    setUserDetails((pre) => {
+      return {
+        ...pre,
+        [cname]: cvalue,
+      }
+    })
+  }
+
+  const processlogin = () => {
+    axios
+      .post('http://localhost:8000/api/login', userDetails)
+      .then((data) => {
+        console.log(data)
+        if (data.data.status == 'true') {
+          // alert(data.data.message)
+          localStorage.setItem('token', JSON.stringify({ token: data.data.token }))
+          uNavigate('/dashboard')
+        } else {
+          alert('please login again')
+        }
+      })
+      .catch((err) => {
+        // console.log(err)
+        if (err.response.status == 401) {
+          alert(err.response.data.message)
+        }
+      })
+  }
+
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -32,7 +71,13 @@ const Login = () => {
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput
+                        placeholder="email"
+                        autoComplete="email"
+                        value={userDetails.email}
+                        name={'email'}
+                        onChange={onUserDetailChange}
+                      />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -42,11 +87,14 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        value={userDetails.password}
+                        name={'password'}
+                        onChange={onUserDetailChange}
                       />
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
+                        <CButton color="primary" className="px-4" onClick={processlogin}>
                           Login
                         </CButton>
                       </CCol>
