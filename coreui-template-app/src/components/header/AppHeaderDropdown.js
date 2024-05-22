cc956c0d11a17cd4d5eaf49c8dc75b337fd4f975
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-05-22 14:51:47 +0500

    updating redux to support user profile

--- coreui-template-app/src/components/header/AppHeaderDropdown.js
+++ coreui-template-app/src/components/header/AppHeaderDropdown.js
@@ -24,11 +24,15 @@ import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
 
+import { useDispatch, useSelector } from 'react-redux'
+
 const AppHeaderDropdown = () => {
+  
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        {/* <CAvatar src={avatar8} size="md" /> */}
+        <CAvatar color="secondary" textColor='white' size='lg'>ahsan</CAvatar>        
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
--- coreui-template-app/src/store.js
+++ coreui-template-app/src/store.js
@@ -1,11 +1,15 @@
 import { legacy_createStore as createStore } from 'redux'
+import { combineReducers } from 'redux'
 
 const initialState = {
-  sidebarShow: true,
-  theme: 'light',
+  sidebarShow: false,
+  theme: 'dark',
 }
 
 const changeState = (state = initialState, { type, ...rest }) => {
+  console.log(`change State ${JSON.stringify(state)}`)
+  console.log(`type:${type}`)
+  // console.log(`rest:${{...rest}}`)
   switch (type) {
     case 'set':
       return { ...state, ...rest }
@@ -14,5 +18,32 @@ const changeState = (state = initialState, { type, ...rest }) => {
   }
 }
 
+const initialSignin = {
+  token: undefined,
+  name: 'Login',
+  email: undefined,
+}
+
+const siginChange = (state = initialSignin, {action,...rest}) => {
+  console.log(`signin ${JSON.stringify(state)}`)
+  console.log(`action is ${action}`);
+  switch (action) {
+    case 'login':
+      state.token = action.token
+      state.name = action.name
+      state.email = action.email
+      return state
+    case 'logout':
+      state.token = undefined
+      state.name = undefined
+      state.name = undefined
+      return state
+    default:
+      return state
+  }
+}
+
+// const store = createStore(combineReducers({changeState}))
 const store = createStore(changeState)
 export default store
+// export {store2}
